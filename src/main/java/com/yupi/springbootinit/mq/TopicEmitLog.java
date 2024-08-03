d6466c885d10e3847d9a83edf7a9253304f35ba3
melon <1148363368@qq.com>
2024-08-03 20:01:19 +0800

    rabbitMQ demo

---
+++ src/main/java/com/yupi/springbootinit/mq/TopicEmitLog.java
@@ -0,0 +1,34 @@
+package com.yupi.springbootinit.mq;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Scanner;
+
+public class DirectEmitLog {
+
+    private static final String EXCHANGE_NAME = "direct";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+            String message = "";
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("输入消息：");
+                message = scanner.nextLine();
+                System.out.println("输入路由键：");
+                String routingKey = scanner.nextLine();
+                channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+                System.out.println(routingKey + " Sent '" + message + "'");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/yupi/springbootinit/mq/TopicReceiveLogs.java
@@ -0,0 +1,35 @@
+package com.yupi.springbootinit.mq;
+
+import com.rabbitmq.client.*;
+
+public class DirectReceiveLogs {
+    private static final String EXCHANGE_NAME = "direct";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+
+        Channel channel1 = connection.createChannel();
+        channel1.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName1 = "one_queue";
+        channel1.queueDeclare(queueName1, true, false, false, null);
+        channel1.queueBind(queueName1, EXCHANGE_NAME, "xiaoyu");
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(delivery.getEnvelope().getRoutingKey() + " [x] Received '" + message + "'");
+        };
+        channel1.basicConsume(queueName1, true, deliverCallback, consumerTag -> { });
+
+
+        Channel channel2 = connection.createChannel();
+        channel2.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName2 = "two_queue";
+        channel1.queueDeclare(queueName2, true, false, false, null);
+        channel2.queueBind(queueName2, EXCHANGE_NAME, "xiaopi");
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        channel2.basicConsume(queueName2, true, deliverCallback, consumerTag -> { });
+    }
+}
