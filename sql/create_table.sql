26898625f756e21440a1d44a5f38bb12fcf0f06d
MELON <1148363368@qq.com>
2024-08-21 22:17:40 +0800

    更新 DDL

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -25,17 +25,21 @@ create table if not exists user
 
 
 -- 图表信息表
-create table if not exists chart
+create table chart
 (
-    id           bigint auto_increment comment 'id' primary key,
-    goal				 text null comment'分析目标',
-    name             varchar(128) null comment '名称',
-    chartData		 text null comment'图表数据',
-    chartType		 varchar(128) null comment'图表类型',
-    genChart		 text null comment'生成的图表数据',
-    genResult		 text null comment'生成的分析结论',
-    userId			 bigint null comment'创建用户 id',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除'
-) comment '图表信息表' collate = utf8mb4_unicode_ci;
+    id          bigint auto_increment comment 'id'
+        primary key,
+    name        varchar(128)                           null comment '名称',
+    goal        text                                   null comment '分析目标',
+    chartData   text                                   null comment '图表数据',
+    chartType   varchar(128)                           null comment '图表类型',
+    genChart    text                                   null comment '生成的图表数据',
+    genResult   text                                   null comment '生成的分析结论',
+    userId      bigint                                 null comment '创建用户 id',
+    status      varchar(128) default 'wait'            not null comment 'wait, running, succeed, failed',
+    execMessage text                                   null comment '图表ai生成执行过程信息',
+    createTime  datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint      default 0                 not null comment '是否删除'
+)
+    comment '图表信息表' collate = utf8mb4_unicode_ci;
